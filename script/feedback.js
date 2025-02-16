ce172219446767b2cd44424b79ad1285c6a0acbd
Houston Braga <houstonliverboy123@gmail.com>
2025-02-16 15:49:44 -0300

    feat: edit copy workshop and feedback

--- script/feedback.js
+++ script/feedback.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const reviews = [
       {
-        feedback: "Fazer o curso técnico de Administração na Avance Cursos foi uma experiência incrível! As aulas são  dinâmicas e voltadas para o mercado de trabalho. Consegui aplicar todos os aprendizados no meu estágio e já estou vendo os resultados! A equipe é super capacitada e pronta para ajudar. Se você busca um curso que prepara para o futuro, não perca tempo, venha para a Avance!",
+        feedback: "Fazer o curso de Administração na Avance Cursos foi uma experiência incrível! As aulas são  dinâmicas e voltadas para o mercado de trabalho. Consegui aplicar todos os aprendizados no meu estágio e já estou vendo os resultados! A equipe é super capacitada e pronta para ajudar. Se você busca um curso que prepara para o futuro, não perca tempo, venha para a Avance!",
         name: "Marcos da silva",
-        role: "Curso Técnico de Administração",
+        role: "Curso Profissionalizante de Administração",
         image: "assets/member.jpg",
         rating: 4.5
       },
       {
-        feedback: "Concluir o curso técnico de Marketing na Avance Cursos foi a melhor decisão que tomei! O conteúdo é atualizado e aplicável, o que me fez entender de verdade como o mercado funciona. A estrutura da escola é ótima com laboratórios de última geração. Se você quer se destacar no mundo do marketing, não tem lugar melhor! Super recomendo!",
+        feedback: "Concluir o curso de Marketing na Avance Cursos foi a melhor decisão que tomei! O conteúdo é atualizado e aplicável, o que me fez entender de verdade como o mercado funciona. A estrutura da escola é ótima com laboratórios de última geração. Se você quer se destacar no mundo do marketing, não tem lugar melhor! Super recomendo!",
         name: "Ana Souza",
         role: "Curso Profissionalizante de Marketing",
         image: "assets/member3.jpeg",
